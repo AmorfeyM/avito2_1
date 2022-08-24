6a1bdf98516c51dafe829a290d3b0be1e3ed5d9b
Sergey Popov <popovsv1987@gmail.com>
2022-08-24 12:37:55 +0300

    - add ShopMapperTest

--- src/test/java/com/amr/project/converter/ShopMapperTest.java
+++ src/test/java/com/amr/project/converter/ShopMapperTest.java
@@ -2,30 +2,79 @@ package com.amr.project.converter;
 
 import com.amr.project.model.dto.ShopDto;
 import com.amr.project.model.entity.Shop;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ShopMapperTest {
-    @Autowired
+    
     private ShopMapper shopMapper;
+    private Shop shop;
+    private ShopDto shopDto;
+    private List<Shop> shopList = new ArrayList<>();
+    private List<ShopDto> shopDtoList = new ArrayList<>();
 
-    @Test
-    void toModel() {
-        ShopDto dto = ShopDto.builder()
+    @Autowired
+    public ShopMapperTest(ShopMapper shopMapper) {
+        this.shopMapper = shopMapper;
+    }
+    @BeforeEach
+    public void setup() {
+        shopDto = ShopDto.builder()
                 .id(1L)
                 .description("description")
                 .name("Vasya")
                 .email("tets@mail.com")
                 .build();
+        shop = Shop.builder()
+                .id(1L)
+                .description("description")
+                .name("Vasya")
+                .email("tets@mail.com")
+                .build();
+        shopList.add(shop);
+        shopDtoList.add(shopDto);
 
-        Shop entity = shopMapper.toModel(dto);
+    }
 
-        assertEquals(dto.getId(), entity.getId());
-        assertEquals(dto.getDescription(), entity.getDescription());
-        assertEquals(dto.getName(), entity.getName());
-        assertEquals(dto.getEmail(), entity.getEmail());
+    @Test
+    void toModel() {
+
+        Shop entity = shopMapper.toModel(shopDto);
+
+        assertEquals(shopDto.getId(), entity.getId());
+        assertEquals(shopDto.getDescription(), entity.getDescription());
+        assertEquals(shopDto.getName(), entity.getName());
+        assertEquals(shopDto.getEmail(), entity.getEmail());
+    }
+
+    @Test
+    void toDto() {
+        ShopDto entity = shopMapper.toDto(shop);
+
+        assertEquals(shop.getId(), entity.getId());
+        assertEquals(shop.getDescription(), entity.getDescription());
+        assertEquals(shop.getName(), entity.getName());
+        assertEquals(shop.getEmail(), entity.getEmail());
+    }
+
+
+    @Test
+    void toDtoList() {
+        List<ShopDto> entityList = shopMapper.toDtoList(shopList);
+        assertEquals(shopList.get(0).getEmail(), entityList.get(0).getEmail());
+
+    }
+
+    @Test
+    void toModelList() {
+        List<Shop> entityList = shopMapper.toModelList(shopDtoList);
+        assertEquals(shopDtoList.get(0).getEmail(), entityList.get(0).getEmail());
     }
 }
