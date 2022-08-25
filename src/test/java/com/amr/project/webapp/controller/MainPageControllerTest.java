fa27f949691ace5c04cac9d56a35529cac591203
Sergey Popov <popovsv1987@gmail.com>
2022-08-25 12:57:06 +0300

    initial commit

--- src/test/java/com/amr/project/webapp/controller/MainPageControllerTest.java
+++ src/test/java/com/amr/project/webapp/controller/MainPageControllerTest.java
@@ -1,42 +1,30 @@
 package com.amr.project.webapp.controller;
 
-import com.amr.project.converter.ItemMapper;
-import com.amr.project.converter.ShopMapper;
-import com.amr.project.service.abstracts.ShopService;
-import com.amr.project.service.abstracts.ShopShowcaseService;
 import com.amr.project.service.impl.MainPageServiceImpl;
-import org.checkerframework.checker.units.qual.A;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc()
 //@ActiveProfiles("dev")
 public class MainPageControllerTest {
-
-    private MockMvc mockMvc;
-
-    private MainPageServiceImpl mainPageService;
     @Autowired
-    public MainPageControllerTest(MockMvc mockMvc, MainPageServiceImpl mainPageService) {
-        this.mockMvc = mockMvc;
-        this.mainPageService = mainPageService;
-    }
+    private MockMvc mockMvc;
+    @MockBean
+    private MainPageServiceImpl mainPageService;
 
     @Test
     public void shouldCreateMock() {
@@ -51,7 +39,10 @@ public class MainPageControllerTest {
     }
 
     @Test
-    void getSalesHistory() {
-
+    void getSalesHistory() throws Exception {
+        mockMvc.perform(get("/sales"))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().contentType("text/html"))
+                .andExpect(forwardedUrl("/salesHistory"));
     }
 }
